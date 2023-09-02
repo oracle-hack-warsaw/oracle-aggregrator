e73ea59f9d546614d8b37b78b083ec0616884589
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-09-02 16:10:13 +0200

    inventory draft

--- packages/nextjs/pages/oracles.tsx
+++ packages/nextjs/pages/oracles.tsx
@@ -22,9 +22,10 @@ const Oracles: NextPage = () => {
           <table className="w-full">
             <thead>
               <tr className="text-left border-b">
-                <th className="p-4">Oracle Provider</th>
-                <th className="p-4">Feed</th>
-                <th className="p-4">Last Updated</th>
+                <th className="p-4 w-1/4">Oracle Provider</th>
+                <th className="p-4 w-1/6">Feed</th>
+                <th className="p-4 w-1/6">Last Updated</th>
+                <th className="p-4 w-1/6">Mint Price</th>
                 <th className="p-4 w-1/4">Action</th>
               </tr>
             </thead>
@@ -46,14 +47,14 @@ const Oracles: NextPage = () => {
                         <td className="p-4 border-b-2">
                           {moment(feed.lastUpdatedLong).fromNow()}
                         </td>
+                        <td className="p-4 border-b-2">
+                            {feed.displayMintPrice}
+                        </td>
                         <td className="p-4 border-b-2 flex justify-between items-center">
                           <button onClick={doMint} className="btn btn-accent w-1/2 relative mr-2">
                             Mint
-                            <span className="absolute bottom-0 left-0 right-0 text-xs bg-opacity-70 p-1">
-                              {feed.displayMintPrice}
-                            </span>
                           </button>
-                          <button onClick={doRent} className="btn btn-secondary w-1/2" disabled>
+                          <button onClick={doRent} className="btn btn-secondary w-1/2 relative" disabled>
                             <span className="tooltip ml-2" title="Minimal design - will work in future">Rent</span>
                           </button>
                         </td>
