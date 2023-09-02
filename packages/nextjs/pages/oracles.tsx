8ede33dc6bff05737c9405c4416405edabc303c3
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-09-02 15:36:17 +0200

    inventory final draft

--- packages/nextjs/pages/oracles.tsx
+++ packages/nextjs/pages/oracles.tsx
@@ -16,53 +16,55 @@ const Oracles: NextPage = () => {
   return (
       <>
         <MetaHeader title="G.O.A.T | Oracles" description="Available Oracles.">
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree&display=swap" rel="stylesheet" />
+          {/* ... */}
         </MetaHeader>
-        <div data-theme="oracles">
-          <div className="container mx-auto p-8">
-            <table className="w-full">
-              <thead>
-                <tr>
-                  <th className="p-4 border-b-2">Provider</th>
-                  <th className="p-4 border-b-2">Feed Name</th>
-                  <th className="p-4 border-b-2">Price</th>
-                  <th className="p-4 border-b-2">Last Updated</th>
-                  <th className="p-4 border-b-2">Address</th>
-                  <th className="p-4 border-b-2">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                {oracleData.map((oracle, index) => (
-                    oracle.feeds.map((feed, fIndex) => (
-                        <tr key={`${index}-${fIndex}`}>
-                          <td className={`p-4 border-b-2 ${oracle.cssClass}`}>
-                            <div className="p-2 rounded-lg">
-                              {oracle.displayName}
-                            </div>
-                          </td>
-                          <td className="p-4 border-b-2">{feed.displayName} ({feed.currentFeedPrice})</td>
-                          <td className="p-4 border-b-2">{feed.displayMintPrice}</td>
-                          <td className="p-4 border-b-2">
-                            {moment(feed.lastUpdatedLong).fromNow()}
-                          </td>
-                          <td className="p-4 border-b-2">{feed.address}</td>
-                          <td className="p-4 border-b-2 flex justify-between">
-                            <button onClick={doMint} className="btn btn-accent w-1/2 mr-2">Mint</button>
-                            <button onClick={doRent} className="btn btn-secondary w-1/2" disabled>
-                              Rent
-                              <span className="tooltip ml-2" data-tip="Minimal design - will work in future">?</span>
-                            </button>
-                          </td>
-                        </tr>
-                    ))
-                ))}
-              </tbody>
-            </table>
-          </div>
+        <div className="container mx-auto p-8 w-full">
+          <table className="w-full">
+            <thead>
+              <tr className="text-left border-b">
+                <th className="p-4">Oracle Provider</th>
+                <th className="p-4">Feed</th>
+                <th className="p-4">Last Updated</th>
+                <th className="p-4">Address</th>
+                <th className="p-4 w-1/4">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {oracleData.map((oracle, oIndex) => (
+                  oracle.feeds.map((feed, fIndex) => (
+                      <tr key={`${oIndex}-${fIndex}`}>
+                        <td className={`p-4 border-b-2 ${oracle.cssClass}`}>
+                          <div className="p-2 rounded-lg">
+                            {oracle.displayName}
+                          </div>
+                        </td>
+                        <td className="p-4 border-b-2">
+                          <div className="font-bold">{feed.displayName}</div>
+                          <div className="text-sm mt-1">{feed.currentFeedPrice}</div>
+                        </td>
+                        <td className="p-4 border-b-2">
+                          {moment(feed.lastUpdatedLong).fromNow()}
+                        </td>
+                        <td className="p-4 border-b-2 text-xs">{feed.address}</td>
+                        <td className="p-4 border-b-2 flex justify-between items-center">
+                          <button onClick={doMint} className="btn btn-accent w-1/2 relative mr-2">
+                            Mint
+                            <span className="absolute bottom-0 left-0 right-0 text-xs bg-opacity-70 p-1">
+                              {feed.displayMintPrice}
+                            </span>
+                          </button>
+                          <button onClick={doRent} className="btn btn-secondary w-1/2" disabled>
+                            <span className="tooltip ml-2" title="Minimal design - will work in future">Rent</span>
+                          </button>
+                        </td>
+                      </tr>
+                  ))
+              ))}
+            </tbody>
+          </table>
         </div>
       </>
   );
-}
+};
 
 export default Oracles;
